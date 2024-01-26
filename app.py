92ed38347956316188c9f0079bd1560f45a44547
IshankSharmaGithub <ishank.sharma.github@gmail.com>
2024-01-26 13:47:02 +0530

    code

--- app.py
+++ app.py
@@ -67,9 +67,22 @@ def connect(auth):
         return
     join_room(room)
     send({"name":name,"message":"has entered the room"},to=room)
-    rooms[room]["member"]+=1
+    rooms[room]["members"]+=1
     print(f"{name} joined room {room}" )
 
+@socketio.on("disconnect")
+def disconnect():
+    room=session.get("room")
+    name=session.get("name")
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]["members"]-=1
+        if rooms[room]["members"] <=0:
+            del rooms[room]
+    
+    send({"name":name,"message":"has left the room"},to=room)
+    print(f"{name} has left the room")
 
 if __name__=="__main__":
     socketio.run(app, debug=True)
